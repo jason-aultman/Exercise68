7add0ed8635a22185a7a1887498038a6ec968c6d
jason-aultman <jason.aultman@gmail.com>
2020-08-11 21:30:43 -0400

    initial commit

---
+++ FileHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Exercise68
+{
+    public static class FileHandler
+    {
+        public static Dictionary<string, string> getDictionary(Dictionary<string, string> dictionaryList, string path)
+        {
+            
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string srStream = sr.ReadLine();
+                    if (srStream != null)
+                    {
+                        var srStreamOne =srStream.Split(',',2,StringSplitOptions.None);
+                        dictionaryList.Add(srStreamOne[0], srStreamOne[1]);
+                    }
+                }
+            }
+
+            return dictionaryList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\jason\RiderProjects\Exercise68\dictionary.txt";
+            Dictionary<string, string> translations = new Dictionary<string, string>();
+            translations=FileHandler.getDictionary(translations,path);
+            do
+            {
+                string englishWord = GetWordInEnglish();
+                string spanishWord = GetWordInSpanish(englishWord, translations);
+                if (spanishWord != null)
+                {
+                    Console.WriteLine($"{englishWord} in spanish is {spanishWord}");
+                }else Console.WriteLine($"{englishWord} cannot be translated.");
+            } while (DoAgain());
+        }
+
+        private static bool DoAgain()
+        {
+            Console.WriteLine("Would you like to go again? y/n");
+            var answer = Console.ReadLine();
+            if (answer.ToLower().StartsWith("y"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static string GetWordInEnglish()
+        {
+            Console.WriteLine("Enter a word in English: ");
+            var englishWord = Console.ReadLine();
+            return englishWord;
+        }
+
+        public static string GetWordInSpanish(string englishWord, Dictionary<string, string> translations)
+        {
+            return translations.GetValueOrDefault(englishWord, null);
+        }
+    }
+}
